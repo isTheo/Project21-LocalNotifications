dfbad44c5cdfa965d92c8c4dd74893b2913fc990
isTheo <matteohg07@gmail.com>
2024-05-15 19:27:46 +0200

    project21 updated

--- Project21-LocalNotifications/ViewController.swift
+++ Project21-LocalNotifications/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
@@ -17,13 +18,39 @@ class ViewController: UIViewController {
     }
     
     
-    
+    //this method will request permission to the user
     @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
 
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
     }
-
+    
+    
     @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
 
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = UNNotificationSound.default
+
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
     }
     
     
